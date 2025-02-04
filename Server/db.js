8c0d97e737a733af1454f2b2d0b728b9b9839ea0
Asura <eper.karma@gmail.com>
2025-02-04 14:50:34 +0100

    app.js + db.js

---
+++ Server/db.js
@@ -0,0 +1,17 @@
+function connect() {
+    const mysql = require('mysql2');
+    require('dotenv').config();
+    const connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE
+    });
+    connection.connect(function(err){
+        if(err) throw err;
+        console.log('Connected to the MySQL server.');
+    });
+    return connection;
+};
+module.exports = connect;
