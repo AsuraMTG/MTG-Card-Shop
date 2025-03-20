f7f0f7bfbae38dd32825a1c4a64df8aa6509a869
Benyi5 <berenyi.benedek5@gmail.com>
2025-03-20 16:02:02 +0100

    Server v16

--- Server/db_web.js
+++ Server/db_web.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 require('dotenv').config();  // Környezeti változók betöltése a .env fájlból
 
 // Adatbázis kapcsolat létrehozása
-const pool = mysql.createPool({
+/*const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
@@ -12,8 +12,16 @@ const pool = mysql.createPool({
     connectionLimit: 10,  // Max. 10 egyidejű kapcsolat
     queueLimit: 0  // Nincs korlátozva a várakozó kapcsolatok száma
 });
-
-
+*/
+// MySQL kapcsolat létrehozása
+const pool = mysql.createConnection({
+    host: 'localhost',      // Az adatbázis hosztja
+    user: 'root',           // Az adatbázis felhasználóneve
+    password: '',   // Az adatbázis jelszava
+    database: 'cardshop', // Az adatbázis neve
+    port: 3307
+      
+  });
 
 // Lekérdezés végrehajtása
 function query(sql, params) {
