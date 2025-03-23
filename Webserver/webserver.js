166a1761eeb3109fa74a066036b8aaeefa398c93
Benyi5 <berenyi.benedek5@gmail.com>
2025-03-23 19:12:32 +0100

    Webserver v3

--- Webserver/webserver.js
+++ Webserver/webserver.js
@@ -2,9 +2,57 @@
 import express  from 'express';
 import mysql from 'mysql2';  // Importáljuk a mysql2 csomagot
 import cors from 'cors';
+import bodyParser  from 'body-parser';
+import WebRoutes from './routes/WebRoutes.js';  // Webes végpontok
+import path from 'path';
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Middleware-ek beállítása
+app.use(bodyParser.json()); // JSON formátumú kérés feldolgozása
+app.use(bodyParser.urlencoded({ extended: true })); // URL-enkódolt adatok
+
+
+// DesktopRoutes kell? Mire? 
+
+
+
+// Alapértelmezett route, ha valaki nem talál semmit
+app.get('/', (req, res) => {
+    res.send('Webshop API működik!');
+});
+
+app.get('/image/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const imagePath = path.join(__dirname, 'product_images', filename);
+    console.log(imagePath);
+    res.sendFile(imagePath, (err) => {
+      if (err) {
+        res.status(404).json({ error: 'Kép nem található!' });
+      }
+    });
+});
+
+// Webes végpontok használata (felhasználói végpontok)
+app.use('/routes', WebRoutes);
+
+
+// Adminisztrátori (asztali) végpontok használata
+//app.use('/desktop', DesktopRoutes);             Not defined 
+
+// Hiba kezelő middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Valami hiba történt az alkalmazásban!' });
+});
+
+// Szerver indítása
+const PORT = process.env.PORT || 3000;  // Alapértelmezett port
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
 
 // MySQL kapcsolat létrehozása /wcreate connection
 const pool = mysql.createConnection({
@@ -12,7 +60,7 @@ const pool = mysql.createConnection({
     user: 'root',           // Az adatbázis felhasználóneve
     password: '',           // Az adatbázis jelszava
     database: 'cardshop',   // Az adatbázis neve
-    port: 3307
+    port: 3306
 });
 
 app.get('/web/login', (req, res)=>{
